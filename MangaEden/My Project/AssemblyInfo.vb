bfc3604c815bba9d26187b8843456e79d2c20d5d
midium <matteo.loro@gmail.com>
2013-11-19 17:52:42 +0100

    Added Setup
    Implemented setup for the application

--- MangaEden/My Project/AssemblyInfo.vb
+++ MangaEden/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.1.2.3")> 
-<Assembly: AssemblyFileVersion("0.1.2.3")> 
+<Assembly: AssemblyVersion("0.1.2.5")> 
+<Assembly: AssemblyFileVersion("0.1.2.5")> 
